564b2c3f64e508cc30630dc63feaa48878adc28b
rixment <rixment@gmail.com>
2020-03-17 13:18:23 +0100

    ShareText method added

--- UnityScript/Awu.cs
+++ UnityScript/Awu.cs
@@ -32,5 +32,18 @@ public static class Awu
             return connectionClass.CallStatic<bool>("isAndroidTv", currentActivity);
 #endif
         }
-    }    
+    }
+
+    public static void ShareText(string title, string subject, string text)
+    {
+#if UNITY_EDITOR || !UNITY_ANDROID
+        Debug.Log(string.Format("title: {0}\nsubject: {1}\ntext: {2}", title, subject, text));
+#else
+        AndroidJavaClass connectionClass = new AndroidJavaClass("rixment.awu.Main");
+        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+
+        connectionClass.CallStatic("shareText", currentActivity, title, subject, text);
+#endif
+    }
 }
--- awu/src/main/java/rixment/awu/Main.java
+++ awu/src/main/java/rixment/awu/Main.java
@@ -2,7 +2,9 @@ package rixment.awu;
 
 import android.app.Activity;
 import android.app.UiModeManager;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -10,14 +12,14 @@ import android.util.Log;
 
 public class Main {
 
-    private static final String TAG = "Rixment";
+    private static final String TAG = "rixment.awu";
 
     public static boolean isNetworkAvailable(Activity activity) {
         Log.i(TAG, "isNetworkAvailable");
         ConnectivityManager connectivityMgr =
                 (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
         if(connectivityMgr == null) {
-            Log.i(TAG, "getSystemService(Context.CONNECTIVITY_SERVICE) == null");
+            Log.w(TAG, "getSystemService(Context.CONNECTIVITY_SERVICE) == null");
             return true;
         }
         NetworkInfo activeNetwork = connectivityMgr.getActiveNetworkInfo();
@@ -29,11 +31,25 @@ public class Main {
         UiModeManager uiModeManager =
                 (UiModeManager) activity.getSystemService(Context.UI_MODE_SERVICE);
         if(uiModeManager == null) {
-            Log.i(TAG, "getSystemService(Context.UI_MODE_SERVICE) == null");
+            Log.w(TAG, "getSystemService(Context.UI_MODE_SERVICE) == null");
             return false;
         }
         int modeType = uiModeManager.getCurrentModeType();
         return modeType == Configuration.UI_MODE_TYPE_TELEVISION;
+    }
+
+    public static void shareText(Activity activity, String title, String subject, String text) {
+        try {
+            Log.i(TAG, "shareText");
+            Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+            intent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+            intent.putExtra(android.content.Intent.EXTRA_TEXT, text);
+            intent.setType("text/plain");
+            activity.startActivity(Intent.createChooser(intent, title));
+        } catch (ActivityNotFoundException e) {
+            String msg = e.getMessage();
+            Log.w(TAG, msg != null ? msg : "shareText ActivityNotFoundException thrown");
+        }
     }
 
 }
