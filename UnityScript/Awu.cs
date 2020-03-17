c9cc1cfc5575b3f1691504ca2f57355d98e8aa41
rixment <rixment@gmail.com>
2020-03-17 09:43:09 +0100

    Create Awu.cs

---
+++ UnityScript/Awu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Awu
+{
+    public static bool IsNetworkAvailable
+    {
+        get
+        {
+#if UNITY_EDITOR || !UNITY_ANDROID
+            return true;
+#else
+            AndroidJavaClass connectionClass = new AndroidJavaClass("rixment.awu.Main");
+            AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+
+            return connectionClass.CallStatic<bool>("isNetworkAvailable", currentActivity);
+#endif
+        }
+    }
+
+    public static bool IsAndroidTV
+    {
+        get
+        {
+#if UNITY_EDITOR || !UNITY_ANDROID
+            return false;
+#else
+            AndroidJavaClass connectionClass = new AndroidJavaClass("rixment.awu.Main");
+            AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+
+            return connectionClass.CallStatic<bool>("isAndroidTv", currentActivity);
+#endif
+        }
+    }    
+}
